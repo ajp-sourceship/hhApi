8878507898806ae9844baacd9056946432cf5201
sourceship <artificalpeter@gmail.com>
2021-06-22 18:20:59 -0500

    test

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ import ColorController from './src/Controllers/ColorController'
 var cors = require('cors')
 const app = express();
 
-const port = process.env.PORT || 3200;
 
 app.use(cors())
 app.use(express.json());
 app.use('/color', ColorController)
 
 
+const port = process.env.port || 3200;
 
 app.listen(port, () => {
     console.log(`running on port ${port}`);
