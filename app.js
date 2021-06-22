9c1a0e903afc0ccecb7ba9f6eb148d8e4d9559d2
sourceship <artificalpeter@gmail.com>
2021-06-22 18:36:10 -0500

    test 3

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use('/color', ColorController)
 
 
-const port = process.env.port || 8081;
+const port = process.env.PORT || 8081;
 
 app.listen(port, () => {
     console.log(`running on port ${port}`);
